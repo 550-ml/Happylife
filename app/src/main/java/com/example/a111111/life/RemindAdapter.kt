cd14cc589013f6018b3f4659c2b7c59d5d04d4b2
cloud <365921891@qq.com>
2023-03-03 09:23:09 +0800

    1

--- app/src/main/java/com/example/a111111/life/RemindAdapter.kt
+++ app/src/main/java/com/example/a111111/life/RemindAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.a111111.life
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a111111.databinding.RemindItemBinding
 
-class RemindAdapter(val RemindList :List<Remind>) : RecyclerView.Adapter<RemindAdapter.RemindViewHolder>() {
+class RemindAdapter(private val RemindList :ArrayList<Remind>) : RecyclerView.Adapter<RemindAdapter.RemindViewHolder>() {
 
 
     class RemindViewHolder(binding: RemindItemBinding): RecyclerView.ViewHolder(binding.root) {
@@ -14,6 +15,16 @@ class RemindAdapter(val RemindList :List<Remind>) : RecyclerView.Adapter<RemindA
         val remindTime : TextView = binding.remindTime
         val remindContent : TextView = binding.remindContent
     }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    private var mOnItemClickListener : OnItemClickListener? = null
+    fun setOnItemClickListener(Listener: OnItemClickListener?) {
+        mOnItemClickListener = Listener
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RemindViewHolder {
         val binding = RemindItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
@@ -25,7 +36,21 @@ class RemindAdapter(val RemindList :List<Remind>) : RecyclerView.Adapter<RemindA
         holder.remindTitle.text = remind.title
         holder.remindTime.text = remind.time
         holder.remindContent.text = remind.content
+        holder.itemView.setOnClickListener {
+            mOnItemClickListener?.onItemClick(position)
+        }
     }
+
+
+    fun removeData(position:Int) {
+        RemindList.removeAt(position)
+        //删除动画
+        notifyItemRemoved(position)
+        notifyDataSetChanged();//为了数据同步防止错位
+    }
+
 
     override fun getItemCount(): Int = RemindList.size
 }
+
+
--- app/src/main/java/com/example/a111111/life/TimedReminderActivity.kt
+++ app/src/main/java/com/example/a111111/life/TimedReminderActivity.kt
@@ -1,6 +1,8 @@
 package com.example.a111111.life
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a111111.BaseActivity
 import com.example.a111111.databinding.ActivityTimedReminderBinding
@@ -15,7 +17,8 @@ class TimedReminderActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityTimedReminderBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.recyclerView.adapter = RemindAdapter(remindList)
+        val adapter = RemindAdapter(remindList)
+        binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.btnSend.setOnClickListener {
             val title = binding.edtTitle.text.toString()
@@ -24,12 +27,27 @@ class TimedReminderActivity : BaseActivity() {
             if(title.isNotEmpty()||content.isNotEmpty()||time.isNotEmpty()){
                 val remind = Remind(title, content, time)
                 remindList.add(remind)
-                adapter?.notifyItemInserted(remindList.size-1)
+                adapter.notifyItemInserted(remindList.size-1)
                 binding.recyclerView.scrollToPosition(remindList.size-1)
                 binding.edtTitle.setText("")
                 binding.edtContent.setText("")
                 binding.edtTime.setText("")
             }
         }
+        adapter.setOnItemClickListener(object :RemindAdapter.OnItemClickListener{
+            override fun onItemClick(position: Int) {
+                AlertDialog.Builder(this@TimedReminderActivity).apply {
+                    setTitle("是否要删除此条提醒")
+                    setMessage("")
+                    setCancelable(false)
+                    setPositiveButton("确定") { dialog, which ->
+                        adapter.removeData(position)
+                    }
+                    setNegativeButton("取消") { dialog, which -> }
+                    show()
+                }
+            }
+        }
+        )
     }
 }
